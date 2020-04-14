571fe8965fc51dd66ce26ffbb501e78f80a3bbe1
joaomsv <joaomarcossv@gmail.com>
2020-04-13 23:03:15 -0300

    Initial commit

---
+++ cypress/integration/Tests/CreateUser.js
@@ -0,0 +1,33 @@
+/// <reference types="Cypress" />
+
+describe('first test', function()
+{
+    it('test1',function()
+    {
+        cy.visit("https://automation.mereo.com/");
+        cy.get('#languageSelect').click()
+        cy.get('[value="pt-BR"]').click()
+        cy.wait(1000)
+        cy.get('#txtUser').type('suporte.mereo')
+        cy.get('#txtPsw').type('Ix57y81l*')
+        cy.get('[value="Login"]').click()
+        cy.get('#SettingsMenu').click()
+        cy.get('#PeopleCentral').click()
+        cy.get('.pull-right.mr-0').find('.mr-btn.mr-btn-primary').click()
+        cy.get('#login').type('teste')
+        cy.get('[name="fullName"]').type('João Teste')
+        cy.get('#emailInputGroup').type('asdas@asdas.com')
+        cy.get('.mr-tree-value').click()
+        // cy.get('.mr-tree-results').contains('Default').click()
+        cy.get('div.node.ng-scope').each(($el,index,$list) => {
+            //$el.find('.branch-toggle.material-icons.md-24.ng-binding').click()
+            var text = $el.text()
+            if(text.includes('PRESIDÊNCIA'))
+            {   
+                $el.find('span.node-selector').click()
+                cy.log($el.text())
+            }
+        })
+        cy.get('#userGroupFormGroup select').select('number:1')
+    })
+})
