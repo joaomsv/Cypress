1e0a522597cf630f2258c651b0d54da007fa96ca
joaomsv <joaomarcossv@gmail.com>
2020-05-03 17:22:32 -0300

    add dark mode add prettier code formatter add some aliases unfinished fixing login generator

--- cypress/integration/Tests/CreateUser.js
+++ cypress/integration/Tests/CreateUser.js
@@ -1,47 +1,78 @@
 /// <reference types="Cypress" />
 
-describe('first test', function()
-{
-    it('test1',function()
-    {
-        cy.visit("https://automation.mereo.com/");
-        cy.get('#languageSelect').click()
-        cy.get('[value="pt-BR"]').click()
-        cy.wait(1000)
-        cy.get('#txtUser').type('suporte.mereo')
-        cy.get('#txtPsw').type('Ix57y81l*')
-        cy.get('[value="Login"]').click()
-        cy.get('#SettingsMenu').click()
-        cy.get('#PeopleCentral').click()
-        cy.wait(1000)
-        cy.get('.pull-right.mr-0').find('.mr-btn.mr-btn-primary').click()
-        cy.get('#login').type('teste')
-        cy.get('[name="fullName"]').type('João Teste')
-        cy.get('.mr-form-group').then(($el) => {
-            var login  
-            for (var i = 0;$el.find('.mr-form-feedback.ng-binding.ng-scope').length > 1;i++)
-            {
-                login = 'teste' + i
-                cy.get('#login').clear()
-                cy.get('[name="fullName"]').clear()
-                cy.get('#login').type(login)
-                cy.get('[name="fullName"]').type('João Teste')
-                $el = cy.get('.mr-form-group')
-            }
-        })
-        cy.get('#emailInputGroup').type('asdas@asdas.com')
-        cy.get('.mr-tree-value').click()
-        // cy.get('.mr-tree-results').contains('Default').click()
-        cy.get('div.node.ng-scope').each(($el,index,$list) => {
-            //$el.find('.branch-toggle.material-icons.md-24.ng-binding').click()
-            var text = $el.text()
-            if(text.includes('PRESIDÊNCIA'))
-            {   
-                $el.find('span.node-selector').click()
-                //cy.log($el.text())
-            }
-        })
-        cy.get('#userGroupFormGroup select').select('number:1')
-        //cy.get('.mr-media-left').find('.mr-btn.mr-btn-clean').click()
+describe('first test', function () {
+  it('test1', function () {
+    var userLogin = 'suporte.mereo'
+    var password = 'Ix57y81l*'
+    var login = 'teste'
+    var name = 'João Teste'
+    cy.visit('https://automation.mereo.com/')
+    cy.server()
+    cy.get('#languageSelect').click()
+    cy.get('[value="pt-BR"]').click()
+    cy.wait(1000)
+    cy.get('#txtUser').type(userLogin)
+    cy.get('#txtPsw').type(password)
+    cy.get('[value="Login"]').click()
+    cy.get('#SettingsMenu').click()
+    cy.get('#PeopleCentral').click()
+    cy.get('.pull-right.mr-0').find('.mr-btn.mr-btn-primary').click()
+    cy.route('POST', 'api/services/app/person/ValidateLogin').as(
+      'ValidateLogin'
+    )
+    cy.get('#login').type(login)
+    cy.get('[name="fullName"]').type(name)
+    cy.wait('@ValidateLogin')
+    // cy.get('.mr-form-group').first().then(($e1) => {
+    //     var text = $e1.find('.mr-form-feedback.ng-binding.ng-scope:visible').text()
+    //     for (var i = 0;i < 5 && text.includes('Login já existe.');i++)
+    //     {
+    //         login = 'teste' + i
+    //         cy.get('#login').clear()
+    //         cy.get('[name="fullName"]').clear()
+    //         cy.get('#login').type(login)
+    //         cy.get('[name="fullName"]').type(name)
+    //         cy.get('.mr-form-group').first().then(($e2) => {
+    //             text = ''
+    //             text = $e2.text()
+    //         })
+    //         cy.log(text)
+    //     }
+    // })
+    cy.get('.mr-form-group').first().as('invalidUserCheck')
+    cy.get('@invalidUserCheck')
+      .then($e1 => {
+        var invalidLogin = $e1.text().includes('Login já existe.')
+        for (var i = 0; i < 5 && invalidLogin; i++) {
+          cy.log(invalidLogin)
+          login = 'teste' + i
+          cy.get('#login').clear()
+          cy.get('[name="fullName"]').clear()
+          cy.get('#login').type(login)
+          cy.get('[name="fullName"]').type(name)
+          cy.wait('@ValidateLogin')
+          cy.get('@invalidUserCheck')
+            .then($e2 => {
+              invalidLogin = false
+              invalidLogin = $e2.text().includes('Login já existe.')
+              cy.log(invalidLogin)
+            })
+        }
+      })
+    cy.get('#emailInputGroup').type('asdas@asdas.com')
+    cy.get('.mr-tree-value').click()
+    cy.get('div.node.ng-scope').each(($el, index, $list) => {
+      var text = $el.text()
+      if (text.includes('PRESIDÊNCIA')) {
+        $el.find('span.node-selector').click()
+      }
     })
+    cy.get('#userGroupFormGroup select').select('number:1')
+    // cy.get('.mr-btn.mr-btn-primary.pull-right.ng-binding').click()
+    // cy.get('.toast-message').should('have.text', 'Registro inserido com sucesso.')
+    // cy.get('.toast-message').then(($msg) => {
+    //     var text = $msg.text()
+    //     expect(text).to.equal('Registro inserido com sucesso.')
+    // })
+  })
 })
