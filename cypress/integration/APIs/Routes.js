721089aab92617bc677624f405ae4deb606de272
João Vieira <joao.vieira@mereo.com>
2020-05-13 21:06:27 -0300

    Added a pageObject style file for APIs as a test

---
+++ cypress/integration/APIs/Routes.js
@@ -0,0 +1,51 @@
+/// <reference types="Cypress" />
+
+class Routes {
+  getPostGetCurrentCulture() {
+    return cy.route('POST', 'api/services/app/systemConfig/GetCurrentCulture')
+  }
+  getPostGetLoggedUser() {
+    return cy.route('POST', 'api/services/app/employee/GetLoggedUser')
+  }
+  getPostCheckUserPermission() {
+    return cy.route('POST', 'api/services/app/permission/CheckUserPermission')
+  }
+  getPostGetUserGroups() {
+    return cy.route('POST', 'api/services/app/person/GetUserGroups')
+  }
+  getPostGetLanguages() {
+    return cy.route('POST', 'api/services/app/person/GetLanguages')
+  }
+  getPostGetLanguage() {
+    return cy.route('POST', 'api/services/app/languageLevel/GetLanguage')
+  }
+  getPostGetListCombobox() {
+    return cy.route('POST', 'api/services/app/languageLevel/GetListCombobox')
+  }
+  getPostGetSkillCategoryList() {
+    return cy.route(
+      'POST',
+      'api/services/app/skillCategory/GetSkillCategoryList'
+    )
+  }
+  getPostGetList() {
+    return cy.route('POST', 'api/services/app/person/GetList')
+  }
+  getPostGetTree() {
+    return cy.route('POST', 'api/services/app/areaTree/GetTree')
+  }
+  getPostGetLoggedUserId() {
+    return cy.route('POST', 'api/services/app/areaTree/GetLoggedUserId')
+  }
+  getPostGetSkillKnowledgeList() {
+    return cy.route(
+      'POST',
+      'api/services/app/skillKnowledge/GetSkillKnowledgeList'
+    )
+  }
+  getPostValidateLogin() {
+    return cy.route('POST', 'api/services/app/person/ValidateLogin')
+  }
+}
+
+export default Routes
